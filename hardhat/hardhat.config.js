ab3ab0fd37f14bf76025f1b4ee6a1c487e29e3a5
script8888 <ayomikun204@gmail.com>
2022-09-29 21:38:55 +0100

    thats it

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -7,7 +7,7 @@ const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    rinkeby: {
+    goerli: {
       url: ALCHEMY_API_KEY_URL,
       accounts: [RINKEBY_PRIVATE_KEY],
     },
